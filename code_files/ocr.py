7c746248e2e678313470ee9400ebd6a3e20bdb26
rachitjindal56 <rachitjindal56@gmail.com>
2022-01-31 14:12:26 +0530

    OCR File Added

---
+++ code_files/ocr.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+import cv2
+import pytesseract
+from pytesseract import Output
+
+def text_image(path):
+    
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    
+    result = pytesseract.image_to_string(image,output_type=Output.STRING)
+    return result
+
+
+    
+    
+    
