ebcc8ae3fee642b226ce525175dc6dcee6a3dcfe
rachitjindal56 <rachitjindal56@gmail.com>
2022-01-31 16:16:45 +0530

    Output file

--- code_files/result.py
+++ code_files/result.py
@@ -1,14 +1,21 @@
 import os
-import ocr
 import cv2
 import re
 import string
+from ocr import text_image
+from re_pan_aadhar_match import extract_Aadhaar
+from re_pan_aadhar_match import extract_PAN
+from names_ner import extract_names
 
 def extract():
     path = "C:/Users/Rachit/Onedrive/Desktop/Files/VS/OCR/duEX6.png"
-    text = ocr.text_image(path)
+    text = text_image(path)
     
-    names = 
+    names = extract_names(text)
+    pan_id = extract_PAN(text)
+    aadhaar_id = extract_Aadhaar(text)
+    
+    print(names,pan_id,aadhaar_id)
 
 if __name__ == '__main__':
     extract()    
