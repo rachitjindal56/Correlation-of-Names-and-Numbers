c952b3fdbb25c76bec4b035329ab87781b37d8d4
rachitjindal56 <rachitjindal56@gmail.com>
2022-01-31 14:41:17 +0530

    Result File added

---
+++ code_files/result.py
@@ -0,0 +1,11 @@
+import os
+import ocr
+import cv2
+import re
+import string
+
+def extract():
+    print(ocr.text_image("C:/Users/Rachit/Onedrive/Desktop/Files/VS/OCR/duEX6.png"))
+
+if __name__ == '__main__':
+    extract()    
