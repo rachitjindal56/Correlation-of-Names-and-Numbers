f7ae2289eb0767cb687b8a40f30fb1424e6b2b94
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-01 09:29:57 +0530

    Output Updated

--- code_files/result.py
+++ code_files/result.py
@@ -8,14 +8,32 @@ from re_pan_aadhar_match import extract_PAN
 from names_ner import extract_names
 
 def extract():
-    path = "C:/Users/Rachit/Onedrive/Desktop/Files/VS/OCR/duEX6.png"
-    text = text_image(path)
+    # path = "C:/Users/Rachit/Onedrive/Desktop/Files/VS/OCR/duEX6.png"
+    # text = text_image(path)
+    
+    text = """
+    The Magistrate and Deputy Commissioner has decided that the following people are banned from 
+    any important postitions and their documents are declared as NUll anc void:
+    Rachit R Jindal: BXEPJ6073L 5844 5786 3665,
+    ABCD Gupta: ABCHF5690X and 9001-7898-4336,
+    XYZ Pichai: BJKPI4782R AND 6785-7643-7800
+    """
     
     names = extract_names(text)
     pan_id = extract_PAN(text)
     aadhaar_id = extract_Aadhaar(text)
+
+    sol = []
     
-    print(names,pan_id,aadhaar_id)
+    for name,pan,adh in zip(names,pan_id,aadhaar_id):
+        zipped = {}
+        zipped["Name"] = name
+        zipped["PAN"] = pan
+        zipped["Aadhar"] = adh
+        
+        sol.append(zipped)
+        
+    print(sol)
 
 if __name__ == '__main__':
     extract()    
