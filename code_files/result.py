0d3aa2698cc3d86a6940461fc32be458c1ee1f41
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-01 14:55:59 +0530

    Output file module update

--- code_files/result.py
+++ code_files/result.py
@@ -6,28 +6,21 @@ from ocr import text_image
 from re_pan_aadhar_match import extract_Aadhaar
 from re_pan_aadhar_match import extract_PAN
 from names_ner import extract_names
+from output_phrasing import final_output
 
-def extract(path:str):
-    path = "C:/Users/Rachit/Onedrive/Desktop/Files/VS/OCR/duEX6.png"
+def extract():
+    path = "C:/Users/Rachit/Downloads/sample_pan.jpg"
     text = text_image(path)
-    
     
     names = extract_names(text)
     pan_id = extract_PAN(text)
     aadhaar_id = extract_Aadhaar(text)
 
-    sol = []
+    sol = final_output(names,pan_id,aadhaar_id)
     
-    for name,pan,adh in zip(names,pan_id,aadhaar_id):
-        zipped = {}
-        zipped["Name"] = name
-        zipped["PAN"] = pan
-        zipped["Aadhar"] = adh
-        
-        sol.append(zipped)
-        
     print(sol)
 
 if __name__ == '__main__':
-    path = input("Enter the path of the image date to be extracted: ")
-    extract(path)
+    # path = input("Enter the path of the image date to be extracted: ")
+    # extract(path)
+    extract()
