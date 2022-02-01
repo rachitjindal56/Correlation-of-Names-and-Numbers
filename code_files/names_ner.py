44bdf1a23db189f56b8fe9423fe024a0547514ba
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-01 14:54:39 +0530

    Name Extraction model Updated

--- code_files/names_ner.py
+++ code_files/names_ner.py
@@ -1,23 +1,20 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
 
-
 def extract_names(text:str)->list:
-    # tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-    # model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    
+    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER-uncased")
+    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER-uncased")
 
-    # nlp = pipeline("ner", model=model, tokenizer=tokenizer)
-    nlp = pipeline("ner", grouped_entities=True)
+    nlp = pipeline("ner", model=model, tokenizer=tokenizer,grouped_entities=True)
+    # nlp = pipeline("ner", grouped_entities=True)
 
     ner_results = nlp(text)
-    
-    if len(ner_results) == 0:
-        return []
     
     names_list = []
     
     for obs in ner_results:
-        if obs['entity_group'] == 'PER':
+        if obs['entity_group'] == 'PER' and len(obs['word']) > 5:
             names_list.append(obs['word'])
     
     return names_list
