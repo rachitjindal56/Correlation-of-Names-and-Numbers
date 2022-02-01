3c873819806242fb66aeef043d88f97c49516c80
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-01 09:29:36 +0530

    Name Extraction

--- code_files/names_ner.py
+++ code_files/names_ner.py
@@ -3,10 +3,21 @@ from transformers import pipeline
 
 
 def extract_names(text:str)->list:
-    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    # tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+    # model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
 
-    nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+    # nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+    nlp = pipeline("ner", grouped_entities=True)
 
     ner_results = nlp(text)
-    return ner_results
+    
+    if len(ner_results) == 0:
+        return []
+    
+    names_list = []
+    
+    for obs in ner_results:
+        if obs['entity_group'] == 'PER':
+            names_list.append(obs['word'])
+    
+    return names_list
