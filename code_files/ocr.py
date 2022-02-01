0ac59c9fc07ed5269f22314ef9dcdc1157ada990
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-01 14:55:23 +0530

    OCR-> Text processing update

--- code_files/ocr.py
+++ code_files/ocr.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
 import cv2
+import re
 import pytesseract
 from pytesseract import Output
 
@@ -9,5 +10,7 @@ def text_image(path:str)->str:
     image = cv2.imread(path)
     image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
     
-    result = pytesseract.image_to_string(image,output_type=Output.STRING)
+    ocr_text = pytesseract.image_to_string(image,output_type=Output.STRING)
+    
+    result = re.sub("\n+"," ",ocr_text).upper()
     return result
