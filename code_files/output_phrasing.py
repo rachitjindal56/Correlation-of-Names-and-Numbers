85a54d256e7072b6117f144f5665df044873a227
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-02 11:24:21 +0530

    Phrasing the output text as dictionary

--- code_files/output_phrasing.py
+++ code_files/output_phrasing.py
@@ -4,7 +4,14 @@ import string
 def final_output(names:list,pan_id:list,aadhar_id:list)->list:
     sol = []
     
-    if len(aadhar_id) == 0:
+    # If the Aadhaar ID and PAN ID are empty then returning the list
+    # with NO ID's
+    if len(pan_id) == 0 and len(aadhar_id) == 0:
+        sol.append("No ID found")
+        
+    # If the Aadhaar ID is empty then returning the list
+    # with Names and PAN ID
+    elif len(aadhar_id) == 0:
         for name,pan in zip(names,pan_id):
             zipped = {}
             zipped["Name"] = name
@@ -13,6 +20,8 @@ def final_output(names:list,pan_id:list,aadhar_id:list)->list:
             
             sol.append(zipped)
     
+    # If the PAN ID is empty then returning the list
+    # with Names and Aadhaar ID
     elif len(pan_id) == 0:
         for name,adh in zip(names,aadhar_id):
             zipped = {}
@@ -22,9 +31,8 @@ def final_output(names:list,pan_id:list,aadhar_id:list)->list:
             
             sol.append(zipped)
     
-    elif len(pan_id) == 0 and len(aadhar_id) == 0:
-        sol.append("No ID found")
-        
+    # If the Aadhaar ID and PAN ID are not empty then returning the list
+    # with Names, PAN and Aadhaar ID's
     else:
         for name,pan in zip(names,pan_id):
             zipped = {}
