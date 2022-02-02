4dd623d2114b4cc24c91bb1575bbc8262bc15011
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-02 11:25:05 +0530

    Aadhaar and PAN extraction file

--- code_files/re_pan_aadhar_match.py
+++ code_files/re_pan_aadhar_match.py
@@ -1,21 +1,31 @@
 import re
 
 def extract_PAN(text:str)->list:
+    
+    # Defining pattern for PAN Card
     pattern = "[A-Z]{3}[ABCFGHLJPTF][A-Z][0-9]{4}[A-Z]"
     
+    # Converting the text to upper case
     text = text.upper()
+    
+    # Finding the PAN Card ID using above pattern
     result = re.findall(pattern,text)
     
+    # If the pattern is not found an empty list is returned
     if len(result) == 0:
         return []
 
     return result
 
 def extract_Aadhaar(text:str)->list:
+    
+    # Defining pattern for Aadhaar Card
     pattern = "\d{4}-\d{4}-\d{4}|\d{4} \d{4} \d{4}"
     
+    # Finding the Aadhaar Card numbers using above pattern
     result = re.findall(pattern,text)
     
+    # If the pattern is not found an empty list is returned
     if len(result) == 0:
         return []
     
