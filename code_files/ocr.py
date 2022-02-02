d34a8298e73ad4b525ef0eb838ce7c1f6f41eedc
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-02 11:23:08 +0530

    OCR Image->Text Updated for Improved Text Processing

--- code_files/ocr.py
+++ code_files/ocr.py
@@ -1,16 +1,32 @@
 import os
+from unittest import result
 from PIL import Image
 import cv2
 import re
 import pytesseract
 from pytesseract import Output
 
+def text_process(text:str)->str:
+    
+    # Removing the extra spaces and new line from the 
+    # extracted text and converting into upper case
+    ocr_text = text.lstrip().rstrip().upper()
+    result = re.sub("\n+"," ",ocr_text)
+    result = re.sub("\s+"," ",result)
+    
+    return result
+
 def text_image(path:str)->str:
     
+    # Reading images using cv2 module and converting the 
+    # extracted image to grayscale for uniformity for 
+    # improved text extraction
     image = cv2.imread(path)
     image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
     
+    # Using Pytesseract for converting image to string
     ocr_text = pytesseract.image_to_string(image,output_type=Output.STRING)
     
-    result = re.sub("\n+"," ",ocr_text).upper()
+    # Text processing
+    result = text_process(ocr_text)
     return result
