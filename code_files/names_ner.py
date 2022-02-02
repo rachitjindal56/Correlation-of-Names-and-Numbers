a5fbd2a6f895fac5c167f9d8258e7162a6da4650
rachitjindal56 <rachitjindal56@gmail.com>
2022-02-02 11:22:13 +0530

    NER Names updated

--- code_files/names_ner.py
+++ code_files/names_ner.py
@@ -3,16 +3,24 @@ from transformers import pipeline
 
 def extract_names(text:str)->list:
     
+    # Using a pre-trained Hugging Face model for extracting the names
+    # by using the NER pipeline
+    
+    # Loading the tokenizer
     tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER-uncased")
+    
+    # Loading the model
     model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER-uncased")
 
+    # Defining the NER pipeline with entities as grouped
     nlp = pipeline("ner", model=model, tokenizer=tokenizer,grouped_entities=True)
-    # nlp = pipeline("ner", grouped_entities=True)
 
+    # Extracting the names
     ner_results = nlp(text)
     
     names_list = []
     
+    # Extracting only the Person name defined as 'PER' by the model
     for obs in ner_results:
         if obs['entity_group'] == 'PER' and len(obs['word']) > 5:
             names_list.append(obs['word'])
